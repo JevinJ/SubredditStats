6c5247809e0af973ff2807bd1aa050e78f5c902e
JevinJ <jevinjones@openmailbox.org>
2017-05-28 20:18:10 -0700

    Update SrStats.py

--- SrStats.py
+++ SrStats.py
@@ -2,8 +2,8 @@ import praw
 import string
 
 reddit = praw.Reddit("bot3")
-subreddit = reddit.subreddit("politics")													#Put subreddit title here as it apprears in url, multiple subs can be read
-																					# by appending '+' between titles, eg "sub1+sub2"
+subreddit = reddit.subreddit("politics")								#Put subreddit title here as it apprears in url, multiple subs can be read
+													# by appending '+' between titles, eg "sub1+sub2"
 with open('blacklist.txt', 'r') as blacklist_raw:
 	blacklist = blacklist_raw.read()
 	blacklist = blacklist.split('\n')
@@ -20,8 +20,8 @@ with open('topics.csv', 'r') as topics_raw:
 	topics = list(filter(None, topics))
 
 def filterBody(text):
-	text = text.split(' ')															#Text filters: Punctuation, undesired words from filter.txt(blacklist),		
-	text = [word.lower() for word in text]											# non alpha words, and <= 1 char and >= char words
+	text = text.split(' ')										#Text filters: Punctuation, undesired words from filter.txt(blacklist),		
+	text = [word.lower() for word in text]								# non alpha words, and <= 1 char and >= char words
 	text = [''.join(ch for ch in word if ch not in string.punctuation) for word in text]																							 																		
 	text = [word for word in text if word not in blacklist and \
 	+ word.isalpha() and len(word) > 1 and len(word) < 15]
@@ -29,8 +29,8 @@ def filterBody(text):
 	
 for submission in subreddit.hot(limit=25):
 	print("In submission: " + str(submission.title))
-	submission.comments.replace_more(limit=0, threshold= 4)							#Can limit how many "More Comments" you request or threshold based on how 
-	comments = submission.comments.list()											# many comments you'll get. 2 seconds per request.
+	submission.comments.replace_more(limit=0, threshold= 4)						#Can limit how many "More Comments" you request or threshold based on how 
+	comments = submission.comments.list()								# many comments you'll get. 2 seconds per request.
 	for comment in comments:
 		if str(comment.author) != "AutoModerator" and str(comment.id) not in polled:			
 			text = filterBody(comment.body)
@@ -48,7 +48,7 @@ for submission in subreddit.hot(limit=25):
 								for x in topics:
 									f.write(x + '\n')
 							break
-					if not any(word in line for line in topics):					#Adding words to file if not in
+					if not any(word in line for line in topics):			#Adding words to file if not in
 						topics.append(word + ',1')
 						with open('topics.csv', 'w', encoding="utf-8") as f:
 							for x in topics:
