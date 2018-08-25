9ea270d2f706617bcd4aca66cd80c7ef26fa6380
JevinJ <jevinjones@openmailbox.org>
2018-08-24 20:49:08 -0700

    Added argparse functionality. Added some feedback on what the program is doing/init arguments made. Add time_now variable in main-loop.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import defaultdict
 from datetime import datetime
 import praw
@@ -7,15 +8,17 @@ import constants
 
 
 class SubredditStats:
-    def __init__(self, bot_name, target, csv_period=1):
+    def __init__(self, **kwargs):
         '''
         :param bot_name: The name/site of the bot as defined in praw.ini
         :param target: Target subreddit(s), eg: 'pcgaming' or 'pcgaming+gaming'
         :param csv_period: Time period in days to generate a full CSV file of results.
         '''
-        self.reddit = praw.Reddit(bot_name)
-        self.target = self.reddit.subreddit(target)
-        self.csv_period = csv_period
+        print(f'Running with commands --name({kwargs["bot_name"]}), --target({kwargs["target"]}),'
+              f' and generating a new csv every {kwargs["csv_period"]} day(s)')
+        self.reddit = praw.Reddit(kwargs['bot_name'])
+        self.target = self.reddit.subreddit(kwargs['target'])
+        self.csv_period = kwargs['csv_period']
         self.word_frequency = defaultdict(int)
         self.user_filter = set(filter(None, fio.load_file('', 'user_filter.txt')))
         self.word_filter = set(filter(None, fio.load_file('', 'word_filter.txt')))
@@ -62,22 +65,31 @@ class SubredditStats:
                 if str(submission.author).lower() not in self.user_filter:
                     self.update_dataset(submission.title)
 
+            time_now = datetime.now()
             #If an hour has passed since start-up/last save, save the word frequencies into a file and wipe it.
-            if (datetime.now() - self.last_dataset_save).seconds >= 3600:
-                pickle_filename = f'{self.last_dataset_save.strftime(constants.TIMESTAMP_FORMAT)}.pickle'
+            if (time_now - self.last_dataset_save).seconds // 3600 >= 1:
+                pickle_filename = f'{self.last_dataset_save.strftime(constants.FILE_TIMESTAMP_FORMAT)}.pickle'
                 fio.save_pickle(self.word_frequency, 'output', pickle_filename)
                 self.word_frequency = defaultdict(int)
                 self.last_dataset_save = datetime.now()
+                print(f'{time_now.strftime(constants.CONSOLE_TIMESTAMP_FORMAT)}' + \
+                      f' Saved {pickle_filename} to /output/')
 
             #If self.csv_period days have passed since start-up/last save, generate a csv and wipe saved pickle files.
-            if (datetime.now() - self.last_csv_generation).seconds // 86400 >= self.csv_period:
-                csv_filename = f'{self.last_csv_generation.strftime(constants.TIMESTAMP_FORMAT)}.csv'
+            if (time_now - self.last_csv_generation).seconds // 86400 >= self.csv_period:
+                csv_filename = f'{self.last_csv_generation.strftime(constants.FILE_TIMESTAMP_FORMAT)}.csv'
                 fio.generate_csv('output', 'output/completedcsv', csv_filename)
                 self.last_csv_generation = datetime.now()
+                print(f'{time_now.strftime(constants.CONSOLE_TIMESTAMP_FORMAT)}' + \
+                      f' Generated {csv_filename} to /output/completedcsv')
 
 
 if __name__ == '__main__':
-    SubredditStats('bot1', 'all')
-
-
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, metavar='BOT_NAME', dest='bot_name', required=True,
+                        help='The name/site of the bot as defined in praw.ini')
+    parser.add_argument('--target', type=str, metavar='SUB_NAME', required=True,
+                        help='Subreddit(s) to target, "+" to use multiple subs, eg: pcgaming or pcgaming+gaming')
+    parser.add_argument('--interval', type=lambda i: abs(int(i)), metavar='CSV_PERIOD', dest='csv_period', default=1,
+                        help='Time interval in days to compile a csv containing word frequencies for that period')
+    SubredditStats(**vars(parser.parse_args()))
