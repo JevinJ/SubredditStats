830986b93afff7245b8eaea29b69bf906dd83696
JevinJ <jevinjones@openmailbox.org>
2018-08-24 19:06:35 -0700

    Add CONSOLE_TIMESTAMP_FORMAT, and rename TIMESTAMP_FORMAT to FILE_TIMESTAMP_FORMAT

--- app/constants.py
+++ app/constants.py
@@ -2,4 +2,5 @@ from string import punctuation
 
 
 PUNCTUATION_TRANS = str.maketrans(dict.fromkeys(punctuation))
-TIMESTAMP_FORMAT = '%Y%b%d_%H.%M.%S'
+FILE_TIMESTAMP_FORMAT = '%Y%b%d_%H.%M.%S'
+CONSOLE_TIMESTAMP_FORMAT = '%I:%M %p:'
